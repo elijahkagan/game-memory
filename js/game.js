053f7bbe1645493cca5b3c447144db8d9be2f4f1
elijahkagan <elija.kagan@toptal.com>
2017-09-10 15:08:13 +0200

    Next iteration of changes

--- js/game.js
+++ js/game.js
@@ -1,20 +1,50 @@
-$( document ).ready( function game() {
-	//console.log( "Starting the game" );
+$( document ).ready( function() {
+	console.log( "Starting The Game!" );
 	
+	//helpers
+	// memoryGame.scrA.hide();
+	// memoryGame.moves = 8 ;
+	// memoryGame.points = 8 ;
+	// memoryGame.gameCardsIteration = 16 ;
+	// memoryGame.updateFinalResults();
+	// memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrC , 0 );
+
+	//Animation
+	var wave = setInterval( waveTheMemory , 6000 );
+	function waveTheMemory() {
+  		$( '.big_letter' ).each( function( i , el ) {
+  			var bigLetter = el ;
+			setTimeout( function() {
+				bigLetter.classList.add( 'waved' );
+				setTimeout( function() {
+					bigLetter.classList.remove( 'waved' );	
+					}, ( i * 50 ) + 50 );
+			}, i * 50 );
+			});
+  	};
+
+	function abortTimer() { // to be called when you want to stop the timer
+  		clearInterval( wave );
+	};
+
 	//Choose difficulty
 	$('.difficulty_button').click(function() {
 		var dataDiff = String( $( this ).attr( 'difficulty' ));
-		//console.log( "Difficulty level: " + dataDiff + "!" );
+		//console.log( "Difficulty level set to: " + dataDiff + "!" );
 		difficultyChoice =  memoryGame.difficulty[ dataDiff ];
 		memoryGame.gameBoard( difficultyChoice );
+		clearInterval( wave );
 	});
 	
 	//Play again
 	$('.play_again').click(function() {
 		memoryGame.resetData();
-		console.log( "Starting the game again - awesome") ;
+		memoryGame.switchGameView( memoryGame.scrC , memoryGame.scrA , 1 );
 	});
-
+	$('.reset_game').click(function() {
+		memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrA , 1 );
+		setTimeout( memoryGame.resetData(), 40 );
+	});
 	//Pick a card
 	$('#game_board').on('click','.memory_card_iteration', function() {
 				var clickedCard = $( this );
--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,8 @@
 var memoryGame = { 
 	// The Memory Game object
 	// Game settings
-	difficulty : { 'kid' : [ 16 , 2 ] , 'expirienced' : [ 72 , 2 ] , 'unicorn' : [ 36, 3 ] } , 
+	difficulty : { 'kid' : [ 16 , 2 ] , 'expirienced' : [ 72 , 2 ] , 'unicorn' : [ 36 , 3 ] } , 
+	hideCardsDelay : 633 ,
 	// The difficulty array is composed of a two elements: [ cardsCount , cardsPairLength ]. The first value, aka cardsCount, states the ammount of all of the cards the game board contains. The second value: 'cardsPairLength', represents the ammount of similar cards a player has to reveal, in one pass, to suceed in singular mamory Game step. Example: in a standard game a pair of cards states for two similar cards but in unicorn game a pair sates for 3 similara games. 
 	
 	// Game UI variable containers
@@ -11,7 +12,8 @@ var memoryGame = {
 	gameBoardElement : $( '#game_board' ),
 	movesContainer : $( '#moves' ),
 	pointsContainer : $( '#points' ),
-	tip : $( '#tip' ),
+	tipContainer : $( '#tip' ),
+	finalResultContainer : $( '.final_result_container' ),
 
 	// Game processing
 	pickedCard : '' ,
@@ -29,40 +31,9 @@ var memoryGame = {
 	// Game results 
 	moves : 0 ,
 	points : 0 ,
+	finalResult : '' ,
 
 	// Game methods
-
-	gameBoard : function( difficultyChoice )  {
-		// function will create a gameboard content withing given constraints and append it to the game placeholder
-		memoryGame.gameCardsIteration =  difficultyChoice[ 0 ];
-		memoryGame.cardsPairLength = difficultyChoice[ 1 ];
-		var localgameCardsIteration = memoryGame.gameCardsIteration;
-		while ( localgameCardsIteration > 0 ) {
-			localgameCardsIteration -= 1 ;
-			var cardTemplate = '<li><a></a></li>' ;
-			var cardID = 'card' + localgameCardsIteration;
-			$( cardTemplate ).addClass( 'card memory_card_iteration' ).attr( 'id' , cardID ).prependTo( memoryGame.gameBoardElement );
-			memoryGame.cardsIdLibrary.push( cardID );
-		};
-
-		memoryGame.craftGameMatrix( symbolsLibrary );
-		
-		memoryGame.setBoardSize();
-
-		memoryGame.scrA.addClass( 'hide' );
-		memoryGame.scrB.removeClass( 'hide' );
-	} ,
-	setBoardSize : function() {
-		var targetBoard = $('.game_child_container');
-		if ( memoryGame.gameCardsIteration == 16 ) {
-			targetBoard.addClass('small');
-		} else if ( memoryGame.gameCardsIteration == 72 ) {
-			targetBoard.addClass('large');
-		} else {
-			targetBoard.addClass('medium');
-		};
-	} ,
-	
 	craftGameMatrix : function(symbolsLibrary) {
 		// B: Creates an arrays wchich elements consists of randomly chosen symbols that are used as a unique library of symbols for the incomming game
 		// I:
@@ -101,14 +72,62 @@ var memoryGame = {
 		//console.log( memoryGame.gameMatrix );
 	} ,
 
+	gameBoard : function( difficultyChoice )  {
+		// function will create a gameboard content withing given constraints and append it to the game placeholder
+		memoryGame.gameCardsIteration =  difficultyChoice[ 0 ];
+		memoryGame.cardsPairLength = difficultyChoice[ 1 ];
+		var localgameCardsIteration = memoryGame.gameCardsIteration;
+		while ( localgameCardsIteration > 0 ) {
+			localgameCardsIteration -= 1 ;
+			var cardTemplate = '<li><a></a></li>' ;
+			var cardID = 'card' + localgameCardsIteration;
+			$( cardTemplate ).addClass( 'card memory_card_iteration' ).attr( 'id' , cardID ).prependTo( memoryGame.gameBoardElement );
+			memoryGame.cardsIdLibrary.push( cardID );
+		};
+		memoryGame.craftGameMatrix( symbolsLibrary );
+		memoryGame.setBoardSize();
+		memoryGame.switchGameView( memoryGame.scrA , memoryGame.scrB , 1 );
+	} ,
+
+	setBoardSize : function() {
+		var targetBoard = $( '.game_child_container' );
+		if ( memoryGame.gameCardsIteration == 16 ) {
+			targetBoard.addClass( 'small' );
+		} else if ( memoryGame.gameCardsIteration == 72 ) {
+			targetBoard.addClass( 'large' );
+		} else {
+			targetBoard.addClass( 'medium' );
+		};
+	} ,
+	
+	switchGameView : function( viewToHide , viewToShow , option ) {
+		viewToHide.addClass( 'move_up' );
+		
+		if ( option == 1) {
+			memoryGame.toggleGameBackgrounds();
+		};
+	
+		setTimeout(function(){
+       		viewToHide.addClass( 'hide' );
+       		viewToHide.removeClass( 'move_up' );
+       		
+       		viewToShow.addClass( 'shifted_down' );
+       		viewToShow.removeClass( 'hide' );
+			setTimeout(function(){
+				viewToShow.removeClass( 'shifted_down' );
+   			}, 0);
+   		}, 200);
+	},	
+
 	selectTheCard : function( clickedCard ) {
-		console.log( memoryGame.cardsSelected );
+		//console.log( memoryGame.cardsSelected );
 		if ( !( clickedCard.hasClass( 'selected' ) )) {
 			clickedCard.addClass( 'selected' );
 			var clickId = clickedCard.attr( 'id' );
 			var clickSymbolClass = memoryGame.gameMatrix[ clickId ];	
 			//console.log( clickSymbolClass );
 			var innerSymbol = clickedCard.find('a');
+			//innerSymbol.fadeIn( 0 );
 			innerSymbol.addClass( clickSymbolClass );
 			memoryGame.cardsSelected.push( clickId );
 			//console.log(memoryGame.cardsSelected);
@@ -117,7 +136,7 @@ var memoryGame = {
 		} else {
 			var innerSymbol = clickedCard.find('a');
 			innerSymbol.addClass( 'nonono' );
-			memoryGame.updateTip("This Card is already revealed!");
+			memoryGame.updateTip( "This Card is already revealed!" );
 			setTimeout(function() {
 					innerSymbol.removeClass( 'nonono' );
 				}, 200);
@@ -125,10 +144,10 @@ var memoryGame = {
 	} ,
 
 	showMatchingCards : function( clickSymbolClass ) {
-		var lists = $('.memory_card_iteration');
+		var lists = $( '.memory_card_iteration' );
 		//console.log(lists);
 			lists.each( function() {
-			var thisCardId = $(this).attr( 'id' );
+			var thisCardId = $( this ).attr( 'id' );
 			var thisSymbolClass = memoryGame.gameMatrix[ thisCardId ];
 			//console.log(thisSymbolClass);
 			if ( thisSymbolClass == clickSymbolClass) {
@@ -139,40 +158,40 @@ var memoryGame = {
 
 	checkThePair : function() {
 		var cardsToMatch = memoryGame.cardsSelected.length ;
-		console.log( "cardsToMatch: " + cardsToMatch );
+		//console.log( "cardsToMatch: " + cardsToMatch );
 		if ( cardsToMatch > 1 ) {
-			console.log( "starting matching process of:" +  cardsToMatch + "elements" );
+			//console.log( "starting matching process of:" +  cardsToMatch + "elements" );
 			var matchingSymbolId = memoryGame.cardsSelected[ 0 ];
 			var matchingSymbolClass = memoryGame.gameMatrix[ matchingSymbolId ];
-			console.log("	Entry Symbol: " + matchingSymbolClass);
+			//console.log("	Entry Symbol: " + matchingSymbolClass);
 			for (idIndex in memoryGame.cardsSelected) {
-				console.log( "	Next Symbol Class: " + memoryGame.cardsSelected[idIndex] );
+				//console.log( "	Next Symbol Class: " + memoryGame.cardsSelected[idIndex] );
 				var nextMatchingidIndex = memoryGame.cardsSelected[ idIndex ] ; 
 				//console.log("	nextMatchingidIndex: " + idIndex);
 				var nextMatchingSymbolClass = memoryGame.gameMatrix[ nextMatchingidIndex ];
-				console.log("	nextMatchingSymbolClass: " + nextMatchingSymbolClass );
+				//console.log("	nextMatchingSymbolClass: " + nextMatchingSymbolClass );
 				if ( matchingSymbolClass != nextMatchingSymbolClass ) {
 					memoryGame.pairingFailed();
 					return;
 				} else {
-					console.log("All revealed cards are the same!")
+					//console.log("All revealed cards are the same!")
 				};
 			};
-			console.log("			cardsToMatch:" + cardsToMatch );	
-			console.log("			matchlength:" + (memoryGame.cardsPairLength) );
+			//console.log("			cardsToMatch:" + cardsToMatch );	
+			//console.log("			matchlength:" + (memoryGame.cardsPairLength) );
 			if ( cardsToMatch == memoryGame.cardsPairLength ) {  
 				memoryGame.thePairMatched();
 				} else {
 				memoryGame.updateTip( "Almost there! Keep on picking!" );
 				};
 		} else {
-		  console.log( "Wow, welcome card, you'r the first one!" );
+		  //console.log( "Wow, welcome card, you'r the first one!" );
 		};
 	} ,
 
 	pairingFailed : function() {
 		memoryGame.updateStats( -1 );
-		console.log("the pair failed in:" + memoryGame.cardsSelected );
+		//console.log("the pair failed in:" + memoryGame.cardsSelected );
 		memoryGame.cardsSelected.forEach( function( content ){
 			//console.log("removing selected class from" + content ); 
 			var thisCard = $( "#" + content );
@@ -180,12 +199,13 @@ var memoryGame = {
 			var thisInnerSymbolClass = memoryGame.gameMatrix[ content ];
 			var innerSymbol = thisCard.find('a');
 			innerSymbol.addClass( 'nonono' );
-			memoryGame.updateTip("This are not a matching cards!");
+			memoryGame.updateTip( "This are not a matching cards!" );
+			//innerSymbol.fadeOut( 300 ); TODO - protection agains fast clicking
 			setTimeout(function() {
 			 	innerSymbol.removeClass( 'nonono' );
 			 	innerSymbol.removeClass( thisInnerSymbolClass );
-			 	//innerSymbol.parrent.removeClass('selected')
-				}, 200);
+			 	//innerSymbol.fadeOut( 300 );  TODO - protection agains fast clicking
+				}, memoryGame.hideCardsDelay );
 		});
 		memoryGame.cardsSelected = [] ;
 	} ,
@@ -203,7 +223,7 @@ var memoryGame = {
 			var innerSymbol = thisCard.find('a');
 			innerSymbol.addClass( 'matched_symbols' );
 			thisCard.addClass( 'matched_cards' );
-			thisCard.removeClass('matching_cards'); //comment it out
+			thisCard.removeClass( 'matching_cards' ); //comment it out
 			thisCard.removeClass( 'memory_card_iteration' );
 			thisCard.removeClass( 'selected' );
 			});
@@ -219,42 +239,106 @@ var memoryGame = {
 		if ( memoryGame.pairsRevealed < memoryGame.gameCardsIteration/memoryGame.cardsPairLength ){
 			//console.log( " continue searching " );
 		} else {
-			memoryGame.updateTip("Bravo! You matched them all!");
+			//memoryGame.updateTip( );
+			memoryGame.updateFinalResults();
+
+			$( '.matched_cards' ).each( function( index ) {
+    			var innerSymbol = $(this).find('a');
+    			setTimeout(function() {
+    				innerSymbol.removeClass( 'matched_symbols' );
+    				innerSymbol.addClass( 'winning_symbols' );
+        		}, index * 66); 
+			});
+
 			setTimeout( function() {
-				memoryGame.scrB.addClass( 'hide' );
-				memoryGame.scrC.removeClass( 'hide' );
-   			}, 2200 );
+				memoryGame.gameBoardElement.removeClass( 'matched_symbols' );
+				memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrC , 0 );
+   			}, 999 );
 		};
 	},
 
 	updateTip : function( message ) {
 		//var tip = $( '#tip' );
-		memoryGame.tip.show();
-		memoryGame.tip.empty();
-		memoryGame.tip.text( message );
+		memoryGame.tipContainer.show();
+		memoryGame.tipContainer.empty();
+		memoryGame.tipContainer.text( message );
 		setTimeout( function() {
-			memoryGame.tip.fadeOut( 800 );
-   			}, 800 );
+			memoryGame.tipContainer.fadeOut( 800 );
+   			}, 999 );
 		} ,
 
 	updateMoves : function() {
 		memoryGame.moves += 1 ;
 		memoryGame.movesContainer.empty();
 		memoryGame.movesContainer.text( 'Moves: ' + memoryGame.moves );
-		console.log("Moves updated to" + memoryGame.moves );
+		//console.log("Moves updated to" + memoryGame.moves );
 		} ,
 
 	updateStats : function( vector ) {		
 		memoryGame.points += vector ;
 		var pointsContainer = $( '#points' );
 		memoryGame.pointsContainer.empty();
-		memoryGame.pointsContainer.text( 'Points: ' + memoryGame.points );	
-		console.log("Points updated to" + memoryGame.points );
+		memoryGame.pointsContainer.text( "Points: " + memoryGame.points );	
+		//console.log("Points updated to" + memoryGame.points );
 		memoryGame.updateMoves();
 		} ,
 
+	toggleGameBackgrounds : function() {
+		var gameBody = $( '.game' );
+		if (gameBody.hasClass( 'game_backgrounds' )) {
+			gameBody.removeClass( 'game_backgrounds' );
+		} else {
+			setTimeout( function() {
+				gameBody.addClass( 'game_backgrounds' );
+   			}, 500 );
+		};
+	},
+
+	updateFinalResults : function () {
+		if ( memoryGame.points == 1 || memoryGame.points == -1 ) {
+				memoryGame.finalResult =  "You collected " + memoryGame.points + " point in " + memoryGame.moves + " moves!" ;
+			} else {
+				memoryGame.finalResult = "You collected " + memoryGame.points + " points in " + memoryGame.moves + " moves!" ;
+			};
+		memoryGame.finalResultContainer.empty();
+		memoryGame.finalResultContainer.text( memoryGame.finalResult );
+		memoryGame.litTheStars();
+	},
+	
+	litTheStars : function() {
+		var starIndex = 0 ;
+		var resultQuality = Math.round(( memoryGame.gameCardsIteration + memoryGame.points)/( memoryGame.moves ) * 100) / 100;
+			if( resultQuality > 0.75 ) {
+				starIndex = 5 ;
+			} else if ( resultQuality > 0.5 ) {
+				starIndex = 4 ;
+			} else if ( resultQuality > 0.3 ) {
+				starIndex = 3 ;
+			} else if ( resultQuality > 0.2 ) {
+				starIndex = 2 ;
+			} else if ( resultQuality > 0.1 ) {
+				starIndex = 1 ;
+			} else {
+				starIndex = 0;
+			};
+		 //console.log( resultQuality , starIndex );
+		// console.log($( '.stars' ));
+		
+		$( '.stars' ).each( function( i ) {
+			if ( i < starIndex) {
+				//console.log(i);
+				var innerSymbol = $( this );//.find('li');
+					innerSymbol.removeClass( 'stars--gold' );	
+					//console.log(innerSymbol);
+					setTimeout( function() {
+						innerSymbol.addClass( 'stars--gold' );
+    				}, i * 633); 
+				};
+			});
+	},
+	
 	resetData : function() {
-		console.log( "Resetting!" );
+		//console.log( "Resetting!" );
 		memoryGame.pickedCard = '' ;
 		memoryGame.symbolsForTheGame = [] ;
 		memoryGame.gameMatrix = {} ;
@@ -269,17 +353,16 @@ var memoryGame = {
 		memoryGame.moves = 0 ;
 		memoryGame.points = 0 ;
 
-		//clean container
+		//clean containers
 		memoryGame.updateTip( '' );
 		memoryGame.pointsContainer.empty();
-		memoryGame.pointsContainer.text( 'Points: ' + memoryGame.points );
+		memoryGame.pointsContainer.text( "Points: " + memoryGame.points );
 		memoryGame.movesContainer.empty();
-		memoryGame.movesContainer.text( 'Moves: ' + memoryGame.moves );
+		memoryGame.movesContainer.text( "Moves: " + memoryGame.moves );
 		memoryGame.gameBoardElement.empty();
-		var targetBoard = $('.game_child_container').attr( 'class' , 'game_child_container' );
-		memoryGame.scrC.addClass( 'hide' );
-		memoryGame.scrA.removeClass( 'hide' );
-		console.log( "Starting the game again - that's awesome!") ;
+		var targetBoard = $( '.game_child_container' ).attr( 'class' , 'game_child_container' ); //TODO
+		// Switch views 
+		//console.log( "Starting the game again - that's awesome!") ;
 		} ,
 
 };
