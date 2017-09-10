c4f57b2a71c6330f7fa960a285deb242679e8054
elijahkagan <elija.kagan@toptal.com>
2017-09-10 16:32:53 +0200

    Added descriptions and file cleaning.

--- js/game.js
+++ js/game.js
@@ -1,15 +1,14 @@
+/** @description: Starts th game when document is loaded. */
 $( document ).ready( function() {
-	console.log( "Starting The Game!" );
-	
-	//helpers
-	// memoryGame.scrA.hide();
-	// memoryGame.moves = 8 ;
-	// memoryGame.points = 8 ;
-	// memoryGame.gameCardsIteration = 16 ;
-	// memoryGame.updateFinalResults();
-	// memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrC , 0 );
+	console.log( "Starting The Game!" ); 
 
-	//Animation
+	/** 
+	* @description: Adds transformation to interface elements in looped style throuh repeting some actions in interval.
+	* @constructor: implemented as a variable so it could easily be reused.
+	* @param: each elelmet from the collection is being processed by nested sequence so as a parameters we use the index of the elelement, element itself and time. 
+	* @returns: a nice, visual wave on the letters of the logo. 
+	* TODO: more complex and funny interaction with letter.
+	*/
 	var wave = setInterval( waveTheMemory , 6000 );
 	function waveTheMemory() {
   		$( '.big_letter' ).each( function( i , el ) {
@@ -21,35 +20,62 @@ $( document ).ready( function() {
 					}, ( i * 50 ) + 50 );
 			}, i * 50 );
 			});
-  	};
+  	}
 
-	function abortTimer() { // to be called when you want to stop the timer
+
+	/** 
+	* @description: Stops the time flow for the interval based animation.
+	* @constructor: 
+	* @param: Interval name. 
+	*/
+	function abortTimer() { 
   		clearInterval( wave );
-	};
+	}
 
-	//Choose difficulty
+
+	/** 
+	* @name: Choice of difficulty
+	* @description: Action handler for the buttons that set up different game modes called: dificulties. Starts proces of game elements. Stops animation of the logo. 
+	* @param: User choice.
+	*/
 	$('.difficulty_button').click(function() {
-		var dataDiff = String( $( this ).attr( 'difficulty' ));
+		var dataDiff = String( $( this ).attr( 'data-difficulty' ));
 		//console.log( "Difficulty level set to: " + dataDiff + "!" );
 		difficultyChoice =  memoryGame.difficulty[ dataDiff ];
 		memoryGame.gameBoard( difficultyChoice );
 		clearInterval( wave );
 	});
 	
-	//Play again
-	$('.play_again').click(function() {
-		memoryGame.resetData();
-		memoryGame.switchGameView( memoryGame.scrC , memoryGame.scrA , 1 );
-	});
-	$('.reset_game').click(function() {
-		memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrA , 1 );
-		setTimeout( memoryGame.resetData(), 40 );
-	});
-	//Pick a card
+
+	/** 
+	* @name: Pick a card - the center of expirience. 
+	* @description: Catches the picked card object and passes it as a parameter to the game engine by the call of the memoryGame object.
+	* @param: User choice / clicked element.
+	*/
 	$('#game_board').on('click','.memory_card_iteration', function() {
 				var clickedCard = $( this );
 				memoryGame.selectTheCard( clickedCard );
 	});
 
-	// TODO - start again in the middle of the game.
+
+	/** 
+	* @description: Resets the game and brings user to the Welcome screen. Starts wave animation Interval 
+	* @param: User choice.
+	*/
+	$('.reset_game').click(function() {
+		memoryGame.switchGameView( memoryGame.scrB , memoryGame.scrA , 1 );
+		setTimeout( memoryGame.resetData(), 40 );
+		wave = setInterval( waveTheMemory , 6000 );
+	});
+
+
+	/** 
+	* @description: Allows player to start the game again by reseting the display to the Welcome screen. Starts wave animation Interval 
+	* @param: User choice.
+	*/
+	$('.play_again_button').click(function() {
+		memoryGame.resetData();
+		memoryGame.switchGameView( memoryGame.scrC , memoryGame.scrA , 1 );
+		wave = setInterval( waveTheMemory , 6000 );
+	});
 });
--- js/memory.js
+++ js/memory.js
@@ -1,11 +1,17 @@
+/** 
+	* @name: Memory Game object.
+	* @description:
+	* @param: Library matching difficutly names with game parameter setting. 
+	  The difficulty array is composed of a two elements: [ cardsCount , cardsPairLength ]. The first value, aka cardsCount, states the ammount of all the cards, that the game board contain. 
+	  The second value: 'cardsPairLength', represents the ammount of similar cards a player has to reveal/match in one game round to suceed in singular mamory Game step. 
+	  Example: in a standard game a pair of cards states for two similar cards but in unicorn difficulty game, a pair sates for  three cards with the same sysmbol. 
+	*/
 var memoryGame = { 
-	// The Memory Game object
-	// Game settings
+	// * Settings
 	difficulty : { 'kid' : [ 16 , 2 ] , 'expirienced' : [ 72 , 2 ] , 'unicorn' : [ 36 , 3 ] } , 
 	hideCardsDelay : 633 ,
-	// The difficulty array is composed of a two elements: [ cardsCount , cardsPairLength ]. The first value, aka cardsCount, states the ammount of all of the cards the game board contains. The second value: 'cardsPairLength', represents the ammount of similar cards a player has to reveal, in one pass, to suceed in singular mamory Game step. Example: in a standard game a pair of cards states for two similar cards but in unicorn game a pair sates for 3 similara games. 
-	
-	// Game UI variable containers
+
+	// * Game UI variable containers
 	scrA : $( '#screen_welcome' ),
 	scrB : $( '#screen_game' ),
 	scrC : $( '#screen_results' ),
@@ -15,65 +21,31 @@ var memoryGame = {
 	tipContainer : $( '#tip' ),
 	finalResultContainer : $( '.final_result_container' ),
 
-	// Game processing
+	// * Game processing 
 	pickedCard : '' ,
 	cardsIdLibrary : [] ,
 	symbolsForTheGame : [] ,
-	gameMatrix : {} ,
+	gameMatrix : {} , // My favourite! :)
 	gameCardsIteration : 0 ,
 	cardsPairLength : 0 ,
-
-	tipMessage : '' , 
-	
 	cardsSelected : [] ,
 	pairsRevealed : 0 ,
 
-	// Game results 
+	// * Game state and results elements
+	tipMessage : '' , 
 	moves : 0 ,
 	points : 0 ,
 	finalResult : '' ,
 
-	// Game methods
-	craftGameMatrix : function(symbolsLibrary) {
-		// B: Creates an arrays wchich elements consists of randomly chosen symbols that are used as a unique library of symbols for the incomming game
-		// I:
-		// O:
-		//console.log(cardCount);
-		var symbolStepCount = memoryGame.gameCardsIteration / memoryGame.cardsPairLength // will provide a propper amount of symbols needed to asign all the ids also its a goal for proper matches
-		// this proportion is setting a proper amount of matches to be found.for gameDepth 2 the ammount of symilar cards pairs 
 
-		while ( symbolStepCount > 0 ) { 
-			// fullfils array of symbols with randomly choosen symbols from symbolsLibrary
-			var rndSymindex = Math.floor( Math.random() * symbolsLibrary.length );
-			var symbol = symbolsLibrary.splice( rndSymindex , 1 )[ 0 ];
-			memoryGame.symbolsForTheGame.unshift( symbol );
-			symbolStepCount -= 1 ;
-		};
-		//console.log( "Crafted the Matrix of symbols" );
-		//console.log( memoryGame.symbolsForTheGame );
+	// * memoryGame methods
 
-		memoryGame.symbolsForTheGame.forEach( function ( symbolIndex ) {
-		//loop to assign n times (on the basis of the cardPairLength ) ids inside one record of th idMatrix
-		//console.log( symbolIndex );
-		var localCardsPairLength = memoryGame.cardsPairLength ;
-		while ( localCardsPairLength > 0 )  {
-			//console.log("target: " + cardsPairLength);
-			var rndIdIndex = Math.floor( Math.random() * memoryGame.cardsIdLibrary.length );				
-			//console.log("rndIdIndex: " + rndIdIndex);
-			var idToSymbol = memoryGame.cardsIdLibrary.splice( rndIdIndex , 1 )[ 0 ];
-			//console.log("idToSymbol: " + idToSymbol);
-			var memoryMatrixCell = { [ idToSymbol ] : symbolIndex };
-			$.extend( memoryGame.gameMatrix , memoryMatrixCell );
-			localCardsPairLength -= 1 ;
-		};
-
-		});
-		//console.log( "Crafted the Game Matrix" );
-		//console.log( memoryGame.gameMatrix );
-	} ,
-
+	/** 
+	* @description: On the basis of the user choice of difficulty level creates a game board of element with a specific ID, than initialise the process of making game variables and cals for game view to show.
+	  Create a gameboard content withing given constraints and append it to the game placeholder
+	* @param: difficultyChoice array describing the ammount of cards in the game and the length of the pair of cards to match in single maching round.
+	*/
 	gameBoard : function( difficultyChoice )  {
-		// function will create a gameboard content withing given constraints and append it to the game placeholder
 		memoryGame.gameCardsIteration =  difficultyChoice[ 0 ];
 		memoryGame.cardsPairLength = difficultyChoice[ 1 ];
 		var localgameCardsIteration = memoryGame.gameCardsIteration;
@@ -87,8 +59,43 @@ var memoryGame = {
 		memoryGame.craftGameMatrix( symbolsLibrary );
 		memoryGame.setBoardSize();
 		memoryGame.switchGameView( memoryGame.scrA , memoryGame.scrB , 1 );
+	} ,	
+
+	/** 
+	* @description: Chooses random, different symbols for the game from the symbols library and appends them to object variable array. 
+	  For each of the chosen symbols, it randomly chooses cards IDs and fullfils a dictionary that describes relation between the card ID and the symbol assigned to it.
+	* @param: Library of symbols, Amount of available cards in a game and the length of the pair.  
+	  Will provide a propper amount of symbols needed to asign all the Card IDs, also its a goal count for proper matches that can be found in a given game.
+	  this proportion is setting a proper amount of matches to be found. for gameDepth 2 the ammount of symilar cards pairs 
+	*/
+	craftGameMatrix : function(symbolsLibrary) {
+		var symbolStepCount = memoryGame.gameCardsIteration / memoryGame.cardsPairLength 
+		while ( symbolStepCount > 0 ) { 
+			// Fullfils array of symbols with randomly choosen symbols from symbolsLibrary
+			var rndSymindex = Math.floor( Math.random() * symbolsLibrary.length );
+			var symbol = symbolsLibrary.splice( rndSymindex , 1 )[ 0 ];
+			memoryGame.symbolsForTheGame.unshift( symbol );
+			symbolStepCount -= 1 ;
+		};
+		//console.log( memoryGame.symbolsForTheGame );
+		memoryGame.symbolsForTheGame.forEach( function ( symbolIndex ) { 
+		//loop to assign n times (on the basis of the cardPairLength ) the symbol in aa array of CardID.
+		var localCardsPairLength = memoryGame.cardsPairLength ;
+		while ( localCardsPairLength > 0 )  {
+			var rndIdIndex = Math.floor( Math.random() * memoryGame.cardsIdLibrary.length );				
+			var idToSymbol = memoryGame.cardsIdLibrary.splice( rndIdIndex , 1 )[ 0 ];
+			var memoryMatrixCell = { [ idToSymbol ] : symbolIndex };
+			$.extend( memoryGame.gameMatrix , memoryMatrixCell );
+			localCardsPairLength -= 1 ;
+		};
+		});
+		//console.log( memoryGame.gameMatrix );
 	} ,
 
+	/** 
+	* @description: On the basis of the user choice it ads the proper CSS class to the game cards container. 
+	* TODO: add an option for user customised game.
+	*/
 	setBoardSize : function() {
 		var targetBoard = $( '.game_child_container' );
 		if ( memoryGame.gameCardsIteration == 16 ) {
@@ -99,18 +106,20 @@ var memoryGame = {
 			targetBoard.addClass( 'medium' );
 		};
 	} ,
-	
+
+	/** 
+	* @description: Switches views on the basis on parameters
+	* @param: Takes in an information about wich screen shoud be hidden and than wich screen should be shown. Also considers an option of turning on beackground class for certain situations.
+	*/
 	switchGameView : function( viewToHide , viewToShow , option ) {
 		viewToHide.addClass( 'move_up' );
-		
 		if ( option == 1) {
 			memoryGame.toggleGameBackgrounds();
 		};
-	
 		setTimeout(function(){
+			// Stops the execution of acrions so the user can expirience a sequence of events
        		viewToHide.addClass( 'hide' );
        		viewToHide.removeClass( 'move_up' );
-       		
        		viewToShow.addClass( 'shifted_down' );
        		viewToShow.removeClass( 'hide' );
 			setTimeout(function(){
@@ -119,19 +128,22 @@ var memoryGame = {
    		}, 200);
 	},	
 
+	/** 
+	* @description: Reasons about the card state, finds proper symbol to display. Starts the proces of matching the cards sysmbols.
+	* @param: The card object clicked by the user.
+	* EXTRA: To start using superpowers please apply the line commented out ( 'THIS LINE' )
+	*/
 	selectTheCard : function( clickedCard ) {
-		//console.log( memoryGame.cardsSelected );
 		if ( !( clickedCard.hasClass( 'selected' ) )) {
 			clickedCard.addClass( 'selected' );
 			var clickId = clickedCard.attr( 'id' );
 			var clickSymbolClass = memoryGame.gameMatrix[ clickId ];	
-			//console.log( clickSymbolClass );
 			var innerSymbol = clickedCard.find('a');
-			//innerSymbol.fadeIn( 0 );
 			innerSymbol.addClass( clickSymbolClass );
 			memoryGame.cardsSelected.push( clickId );
-			//console.log(memoryGame.cardsSelected);
-			memoryGame.showMatchingCards(clickSymbolClass);
+
+			// memoryGame.showMatchingCards(clickSymbolClass); // <<<< THIS LINE 
+			
 			memoryGame.checkThePair();
 		} else {
 			var innerSymbol = clickedCard.find('a');
@@ -143,45 +155,53 @@ var memoryGame = {
 			};
 	} ,
 
+	/** 
+	* @description: After users choice, this one will show all the maching cards for the initial one.
+	* @param: it takes the symbol from the clicked card as a base for the search of other, same cards.
+	*/
 	showMatchingCards : function( clickSymbolClass ) {
 		var lists = $( '.memory_card_iteration' );
-		//console.log(lists);
 			lists.each( function() {
 			var thisCardId = $( this ).attr( 'id' );
 			var thisSymbolClass = memoryGame.gameMatrix[ thisCardId ];
-			//console.log(thisSymbolClass);
 			if ( thisSymbolClass == clickSymbolClass) {
 				$(this).addClass( 'matching_cards' );
 			};
 		});
 	} ,
 
+	/** 
+	* @description: Checks if the symbols of all the cards that are selected contain the same symbol. The maching process is performed for each element of an array that contains certain ammout of selected card ID, if anytime the symbol is not matching the symbol of the first selected card - the array is being emptied and implications are being processed.  
+	* @param: It uses several variables to perform the task by matching IDs with proper symbols through calling names and values from dictionary that were created at the beginning of the game.
+	*/
 	checkThePair : function() {
 		var cardsToMatch = memoryGame.cardsSelected.length ;
-		//console.log( "cardsToMatch: " + cardsToMatch );
 		if ( cardsToMatch > 1 ) {
-			//console.log( "starting matching process of:" +  cardsToMatch + "elements" );
+			// Will start only if the card selected is not the firs one.
+			// Asigning the symbol from the first card.
 			var matchingSymbolId = memoryGame.cardsSelected[ 0 ];
 			var matchingSymbolClass = memoryGame.gameMatrix[ matchingSymbolId ];
-			//console.log("	Entry Symbol: " + matchingSymbolClass);
+
 			for (idIndex in memoryGame.cardsSelected) {
-				//console.log( "	Next Symbol Class: " + memoryGame.cardsSelected[idIndex] );
+				// Looping through the array of selected card Ids
 				var nextMatchingidIndex = memoryGame.cardsSelected[ idIndex ] ; 
-				//console.log("	nextMatchingidIndex: " + idIndex);
+				// Assigning a symbol to match 
 				var nextMatchingSymbolClass = memoryGame.gameMatrix[ nextMatchingidIndex ];
-				//console.log("	nextMatchingSymbolClass: " + nextMatchingSymbolClass );
+
 				if ( matchingSymbolClass != nextMatchingSymbolClass ) {
+					// Matching if the symbols differ 
 					memoryGame.pairingFailed();
 					return;
 				} else {
-					//console.log("All revealed cards are the same!")
+					// Passing the game state for veryfication of next element
 				};
 			};
-			//console.log("			cardsToMatch:" + cardsToMatch );	
-			//console.log("			matchlength:" + (memoryGame.cardsPairLength) );
 			if ( cardsToMatch == memoryGame.cardsPairLength ) {  
+				// Matching if the ammount of the same symbol ids in the selected cards array is equal to the total amount of the cards with the same symbol (in that game/difficulty)
+				//Starts implication for matched cards
 				memoryGame.thePairMatched();
 				} else {
+				// Updates user with an info and allows for selecting another card.
 				memoryGame.updateTip( "Almost there! Keep on picking!" );
 				};
 		} else {
@@ -189,6 +209,10 @@ var memoryGame = {
 		};
 	} ,
 
+	/** 
+	* @description: 
+	* @param: 
+	*/
 	pairingFailed : function() {
 		memoryGame.updateStats( -1 );
 		//console.log("the pair failed in:" + memoryGame.cardsSelected );
@@ -210,6 +234,10 @@ var memoryGame = {
 		memoryGame.cardsSelected = [] ;
 	} ,
 	
+	/** 
+	* @description: 
+	* @param: 
+	*/
 	thePairMatched : function() {
 		//console.log( "The Pair Matched" );
 		memoryGame.updateTip( "Awesome, you found it!" );
@@ -234,6 +262,10 @@ var memoryGame = {
 
 	} ,
 
+	/** 
+	* @description: 
+	* @param: 
+	*/
 	checkIfGameWon : function() {
 		//console.log( memoryGame.pairsRevealed );
 		if ( memoryGame.pairsRevealed < memoryGame.gameCardsIteration/memoryGame.cardsPairLength ){
@@ -257,6 +289,10 @@ var memoryGame = {
 		};
 	},
 
+	/** 
+	* @description: Updates the HTML display with a given message
+	* @param: Message to pass to DOM element
+	*/
 	updateTip : function( message ) {
 		//var tip = $( '#tip' );
 		memoryGame.tipContainer.show();
@@ -267,6 +303,9 @@ var memoryGame = {
    			}, 999 );
 		} ,
 
+	/** 
+	* @description: Updates the game state variable and user interface with new values for moves,
+	*/
 	updateMoves : function() {
 		memoryGame.moves += 1 ;
 		memoryGame.movesContainer.empty();
@@ -274,6 +313,11 @@ var memoryGame = {
 		//console.log("Moves updated to" + memoryGame.moves );
 		} ,
 
+	/** 
+	* @description: Updates the game state variable and user interface with new values for points. I t also trigers a step for updating the game moves.
+	* @param: As point can go up and dovn, there is a vector input to add 1 or -1 to the points value.
+	* TODO: Initially there was some idea for points, as for now, they are not necesary at all.
+	*/
 	updateStats : function( vector ) {		
 		memoryGame.points += vector ;
 		var pointsContainer = $( '#points' );
@@ -283,6 +327,10 @@ var memoryGame = {
 		memoryGame.updateMoves();
 		} ,
 
+	/** 
+	* @description: Because there are backgrounds only in informative parts of the game - this switches the background on or off.
+	* TODO: Add nicer animation on revealing. Check the Chrome problems.
+	*/
 	toggleGameBackgrounds : function() {
 		var gameBody = $( '.game' );
 		if (gameBody.hasClass( 'game_backgrounds' )) {
@@ -294,6 +342,11 @@ var memoryGame = {
 		};
 	},
 
+	/** 
+	* @description: Constructs proper sentence to be displeyed in the final game screen. Also changes view to the final one and starts the winner stars animation. 
+	* @param: memoryGame object variables - points and moves.
+	* TODO: Fine tune the timings for the different game difficulties.
+	*/
 	updateFinalResults : function () {
 		if ( memoryGame.points == 1 || memoryGame.points == -1 ) {
 				memoryGame.finalResult =  "You collected " + memoryGame.points + " point in " + memoryGame.moves + " moves!" ;
@@ -305,6 +358,11 @@ var memoryGame = {
 		memoryGame.litTheStars();
 	},
 	
+	/** 
+	* @description: Sets proper amount of stars displayed as a gold stars in the game results screen.
+	* @param: Takes the ammount of cards (dependent on the difficulty) of the game and the final states of memoryGame results variables. 
+	* TODO: Quality of the result measuring needs equalisation based on the users results. Will require creation of Game results board to keep game rresults records.
+	*/
 	litTheStars : function() {
 		var starIndex = 0 ;
 		var resultQuality = Math.round(( memoryGame.gameCardsIteration + memoryGame.points)/( memoryGame.moves ) * 100) / 100;
@@ -321,48 +379,43 @@ var memoryGame = {
 			} else {
 				starIndex = 0;
 			};
-		 //console.log( resultQuality , starIndex );
-		// console.log($( '.stars' ));
-		
 		$( '.stars' ).each( function( i ) {
+			// Changes class for each star as long as proper amount of stars is lit.
 			if ( i < starIndex) {
-				//console.log(i);
 				var innerSymbol = $( this );//.find('li');
 					innerSymbol.removeClass( 'stars--gold' );	
-					//console.log(innerSymbol);
 					setTimeout( function() {
+						// Sets a brak before user sees the animations.
 						innerSymbol.addClass( 'stars--gold' );
     				}, i * 633); 
 				};
 			});
 	},
-	
+
+
+	/** 
+	* @description: Resets the initial values of memoryGame variables and cleans HTML containers.
+	*/
 	resetData : function() {
-		//console.log( "Resetting!" );
+		// memoryGame variables
 		memoryGame.pickedCard = '' ;
 		memoryGame.symbolsForTheGame = [] ;
 		memoryGame.gameMatrix = {} ;
 		memoryGame.gameCardsIteration = 0 ;
 		memoryGame.cardPairLength = 0 ;
-
 		memoryGame.tipMessage = '' ; 
-
 		memoryGame.cardsSelected = [] ; 
 		memoryGame.pairsRevealed = 0 ;
-
 		memoryGame.moves = 0 ;
 		memoryGame.points = 0 ;
-
-		//clean containers
+		// empting containers
 		memoryGame.updateTip( '' );
 		memoryGame.pointsContainer.empty();
 		memoryGame.pointsContainer.text( "Points: " + memoryGame.points );
 		memoryGame.movesContainer.empty();
 		memoryGame.movesContainer.text( "Moves: " + memoryGame.moves );
 		memoryGame.gameBoardElement.empty();
-		var targetBoard = $( '.game_child_container' ).attr( 'class' , 'game_child_container' ); //TODO
-		// Switch views 
-		//console.log( "Starting the game again - that's awesome!") ;
+		var targetBoard = $( '.game_child_container' ).attr( 'class' , 'game_child_container' );
 		} ,
 
 };
